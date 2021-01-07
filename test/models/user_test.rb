1a94d73234d6b3da387c08fe626b725a09eebdae
Thibaud Maurel <maurel.thibaud@free.fr>
2021-01-07 11:56:34 +0100

    test unit

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns well formatted name" do
+    user = User.new(first_name: "john", last_name: "doe")
+    assert_equal "John Doe", user.full_name
+  end
 end
